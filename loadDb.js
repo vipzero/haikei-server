39d1e86bfe4a42959316c7aa8ff3f032fcded17a
vipzero <hiroflighter@gmail.com>
2020-12-26 16:38:21 +0900

    fix: loadDb

--- loadDb.js
+++ loadDb.js
@@ -3,6 +3,7 @@ const parse = require('csv-parse/lib/sync')
 const songs = {}
 
 const filenames = [
+  './data/test.csv',
   './data/anison.csv',
   './data/game.csv',
   './data/program.csv',
@@ -14,26 +15,22 @@ const filenames = [
 //   return cell.substr(1, cell.length - 2)
 // }
 filenames.forEach((filename) => {
-  // const headerAlias = 'bId,media,animeTitle,opOrEd,spNum,id,title,artist'
+  const headerAlias = 'bId,media,animeTitle,opOrEd,spNum,id,title,artist'
   const lines = fs.readFileSync(filename, 'utf-8').split('\n')
-  // lines[0] = headerAlias
+  lines[0] = headerAlias
   const data = lines.join('\n')
 
   const rows = parse(data, {
-    columns: true,
-    // columns: headerAlias.split(','),
+    trim: true,
+    header: false,
+    skipLinesWithError: true,
   })
+  // console.log(rows)
 
-  rows.forEach((record) => {
-    const { title, artist } = record
-
+  rows.forEach(([, , title, opOrEd, spInfo, songId, songTitle, artist]) => {
     if (!songs[title]) songs[title] = {}
-    songs[title][artist] = record
+    songs[title][artist] = { title, opOrEd, spInfo, songId, songTitle, artist }
   })
 })
-
-console.log(Object.values(songs)[0])
-console.log(Object.values(songs)[1])
-console.log(Object.values(songs)[2])
 
 exports.default = songs
