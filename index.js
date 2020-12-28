e574dcb1198e4a006b9cbc0caad8e1582aa063f4
vipzero <hiroflighter@gmail.com>
2020-12-28 10:51:46 +0900

    refactor module paths

--- index.js
+++ index.js
@@ -1,44 +1,12 @@
 'use strict'
 
-const subscribeIcy = require('./icy').default
-const songs = require('./loadDb').default
-const { saveMusic } = require('./firebase')
-const { getImage } = require('./customImageSearch')
+const subscribeIcy = require('./lib/icy').default
+const { saveMusic } = require('./lib/firebase')
+const { getImage } = require('./lib/customImageSearch')
+const { findSong } = require('./lib/findSong')
 
 const url = process.env.URL
-const trimTail = (s) => s.substring(0, s.length - 1)
 
-function findSong(icy) {
-  const [artist, titleBase] = icy.split(' - ')
-
-  if (!artist || !titleBase) return { icy }
-  const songBase = { artist, title: titleBase, icy }
-
-  let title = titleBase.toLowerCase()
-  let songsByArtist = songs[title]
-
-  // 曲名検索Hitしない場合後ろの文字から削る
-  // console.log(songsByArtist)
-  while (!songsByArtist && title.length >= 4) {
-    title = trimTail(title)
-    songsByArtist = songs[title]
-  }
-
-  if (!songsByArtist) return songBase
-  // console.log(songsByArtist)
-
-  // Hit したアーティスト名を含むものを一つ選ぶ
-  const trimSpace = (s) => s.replace(' ', '').toLowerCase()
-  const isHit = (long, short) => trimSpace(long).indexOf(trimSpace(short)) >= 0
-
-  const findSong = Object.entries(songsByArtist).find(([k]) =>
-    k.split(',').some((part) => isHit(artist, part))
-  )
-  if (!findSong) return songBase
-  // console.log({ song })
-
-  return { ...findSong[1], icy }
-}
 const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec))
 
 function main() {
--- playground.js
+++ playground.js
@@ -1,43 +1,17 @@
-const songs = require('./loadDb').default
+const { findSong } = require('./lib/findSong')
 
-const trimTail = (s) => s.substring(0, s.length - 1)
-
-function findSong(icy) {
-  const [artist, titleBase] = icy.split(' - ')
-
-  if (!artist || !titleBase) return { icy }
-  const songBase = { artist, title: titleBase, icy }
-
-  let title = titleBase.toLowerCase()
-  let songsByArtist = songs[title]
-
-  // 曲名検索Hitしない場合後ろの文字から削る
-  // console.log(songsByArtist)
-  while (!songsByArtist && title.length >= 4) {
-    title = trimTail(title)
-    songsByArtist = songs[title]
-  }
-
-  if (!songsByArtist) return songBase
-  // console.log(songsByArtist)
-
-  // Hit したアーティスト名を含むものを一つ選ぶ
-  const trimSpace = (s) => s.replace(' ', '').toLowerCase()
-  const isHit = (long, short) => trimSpace(long).indexOf(trimSpace(short)) >= 0
-
-  const findSong = Object.entries(songsByArtist).find(([k]) =>
-    k.split(',').some((part) => isHit(artist, part))
-  )
-  if (!findSong) return songBase
-  // console.log({ song })
-
-  return { ...findSong[1], icy }
-}
-
-// Eve - 蒼のワルツ
-
+// うまく取得できなかった icy のテスト
 // const icy = 'UNISON SQUARE GARDEN - Catch up, latency'
+// const icy = 'Eve - 蒼のワルツ'
 const icy =
   'イリス・フレイア(CV.日高里菜)、物部深月(CV.沼倉愛美) - Ray of bullet'
+// const icy = 'テリブルルッキング - 花咲く☆最強レジェンドDays (男祭りver.)'
 
 console.log(findSong(icy))
+
+// const obj = {
+//   '花咲く☆最強レジェンドDays': 'find',
+// }
+
+// console.log(songs['花咲く☆最強レジェンドdays'])
+// console.log(obj['花咲く☆最強レジェンドDays'])
