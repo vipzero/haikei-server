84a295e9c09f771f2c853b883f6db1033b51d9da
vipzero <hiroflighter@gmail.com>
2020-12-30 15:00:33 +0900

    add lyric save

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 'use strict'
 
 const subscribeIcy = require('./lib/icy').default
-const { saveMusic, addHistoryNow } = require('./lib/firebase')
+const { saveMusic, addHistoryNow, getCurrentPlay } = require('./lib/firebase')
 const { getImage } = require('./lib/customImageSearch')
 const { findSong } = require('./lib/findSong')
 const { sleep } = require('./lib/utils')
 const { getAlbum } = require('./lib/itunes')
+const { getLyrics } = require('./lib/jlyricnet')
 // const { spotifySearchSongInfo } = require('./lib/spotify')
 
 const url = process.env.URL
 
-function main() {
+async function main() {
+  const res = await getCurrentPlay()
+  let startPlay = res && res.icy
+
   subscribeIcy(
     url,
     async (icy) => {
+      if (startPlay && startPlay === icy) {
+        // 起動時の重複登録を防ぐ
+        startPlay = false
+        return
+      }
       addHistoryNow(icy)
       const song = findSong(icy)
 
@@ -30,12 +39,15 @@ function main() {
 
       // NOTE: 破壊的で微妙
       const albumInfos = await getAlbum(icy)
+      const lyrics = await getLyrics(song.title, song.artist)
+      const creators = lyrics ? lyrics.creators : {}
+      const lyric = lyrics ? lyrics.lyric : null
 
       console.log(icy)
       console.log(song)
 
       // console.log(imageLinks)
-      saveMusic({ ...song, imageLinks, ...albumInfos })
+      saveMusic({ ...song, imageLinks, ...albumInfos, ...creators }, lyric)
     },
     async () => {
       // change stream retry
