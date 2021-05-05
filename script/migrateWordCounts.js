18b3623de5d92c1fecc09496104c9744b31ec01f
vipzero <hiroflighter@gmail.com>
2021-05-05 23:57:30 +0900

    add migration firestore count

---
+++ script/migrateWordCounts.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { findSong } = require('../lib/findSong')
+const { anaCounts } = require('../lib/wordCounts')
+const { loadAllIcy, setupCount } = require('../lib/firebase')
+
+function wordCounting(icys) {
+  const res = {}
+
+  icys.forEach((icy) => {
+    const song = findSong(icy)
+    const additionals = [song.title]
+
+    if (song.animeTitle) additionals.push(song.animeTitle)
+    const { wordCounts } = anaCounts(icy, additionals)
+
+    Object.entries(wordCounts).forEach(([k, v]) => {
+      res[k] = (res[k] || 0) + v
+    })
+  })
+  console.log(res)
+}
+
+async function main() {
+  const icys = await loadAllIcy()
+  const counts = wordCounting(icys)
+  await setupCount(counts)
+}
+
+main()
