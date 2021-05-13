81ebbadc918d2ac7d7892a1be4c8df35d62cc556
vipzero <hiroflighter@gmail.com>
2021-05-13 19:01:34 +0900

    refactor main

--- index.js
+++ index.js
@@ -11,61 +11,56 @@ import { getLyrics } from './lib/jlyricnet'
 // import { spotifySearchSongInfo } from './lib/spotify'
 
 const url = process.env.URL
+let counts, startPlay
+
+async function receiveIcy(icy) {
+  console.log(icy)
+  if (startPlay && startPlay === icy) {
+    // 起動時の重複登録を防ぐ
+    startPlay = false
+    return
+  } else {
+    addHistoryNow(icy)
+  }
+  const song = findSong(icy)
+  const additionals = [song.title]
+  if (song.animeTitle) additionals.push(song.animeTitle)
+  const { wordCounts, counts: countsNew } = anaCounts(icy, counts, additionals)
+  counts = countsNew
+
+  const imageSearchWord = song.animeTitle ? song.animeTitle : icy
+  const imageLinks = await getImageLinks(imageSearchWord)
+
+  // const spoinfo = spotifySearchSongInfo(song.title, song.artist)
+  // if (spoinfo) song.artwork = spoinfo.artwork
+
+  // NOTE: 破壊的で微妙
+  const albumInfos = await getAlbum(icy)
+  const lyrics = await getLyrics(song.title, song.artist)
+  const creators = lyrics ? lyrics.creators : {}
+  const lyric = lyrics ? lyrics.lyric : null
+
+  console.log(icy)
+  console.log(song)
+
+  // console.log(imageLinks)
+  saveMusic(
+    { ...song, imageLinks, ...albumInfos, ...creators, wordCounts },
+    lyric
+  )
+}
 
 async function main() {
   const res = await getCurrentPlay()
-  let { counts } = await init()
-  let startPlay = res && res.icy
+  counts = await init().counts
+  startPlay = res && res.icy
 
-  subscribeIcy(
-    url,
-    async (icy) => {
-      console.log(icy)
-      if (startPlay && startPlay === icy) {
-        // 起動時の重複登録を防ぐ
-        startPlay = false
-        return
-      } else {
-        addHistoryNow(icy)
-      }
-      const song = findSong(icy)
-      const additionals = [song.title]
-      if (song.animeTitle) additionals.push(song.animeTitle)
-      const { wordCounts, counts: countsNew } = anaCounts(
-        icy,
-        counts,
-        additionals
-      )
-      counts = countsNew
-
-      const imageSearchWord = song.animeTitle ? song.animeTitle : icy
-      const imageLinks = await getImageLinks(imageSearchWord)
-
-      // const spoinfo = spotifySearchSongInfo(song.title, song.artist)
-      // if (spoinfo) song.artwork = spoinfo.artwork
-
-      // NOTE: 破壊的で微妙
-      const albumInfos = await getAlbum(icy)
-      const lyrics = await getLyrics(song.title, song.artist)
-      const creators = lyrics ? lyrics.creators : {}
-      const lyric = lyrics ? lyrics.lyric : null
-
-      console.log(icy)
-      console.log(song)
-
-      // console.log(imageLinks)
-      saveMusic(
-        { ...song, imageLinks, ...albumInfos, ...creators, wordCounts },
-        lyric
-      )
-    },
-    async () => {
-      // change stream retry
-      console.log('finish')
-      await sleep(10 * 1000)
-      main()
-    }
-  )
+  subscribeIcy(url, receiveIcy, async () => {
+    // change stream retry
+    console.log('finish')
+    await sleep(10 * 1000)
+    main()
+  })
 }
 
 main()
