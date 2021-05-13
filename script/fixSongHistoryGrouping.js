73cabd4a63f3c414a9469613c2c041c3f8c84734
vipzero <hiroflighter@gmail.com>
2021-05-13 18:13:15 +0900

    add script grouping

---
+++ script/fixSongHistoryGrouping.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import { fdb } from '../lib/firebase'
+
+async function main() {
+  const snap = await fdb
+    .collection('hist')
+    .doc('2020nematu')
+    .collection('songs')
+    .where('time', '>=', 1619611546000)
+    .get()
+  console.log(snap.docs.length)
+  const batch = fdb.batch()
+
+  snap.docs.forEach((v) => {
+    const d = v.data()
+
+    batch.set(
+      fdb.collection('hist').doc('2021gw').collection('songs').doc(`${d.time}`),
+      d
+    )
+    batch.delete(v.ref)
+  })
+  await batch.commit()
+}
+
+main()
