54b07588b4a7afee8c816306a6271af5716d75a1
vipzero <hiroflighter@gmail.com>
2021-08-07 09:53:56 +0900

    add setup events

---
+++ script/setupEvent.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import { fdb } from '../lib/firebase'
+const eventId = process.env.EVENT_ID
+
+async function main() {
+  if (!eventId) return
+  await fdb.collection('hist').doc(eventId).set({ lasttime: 0 })
+
+  await fdb
+    .collection('song')
+    .doc(eventId)
+    .set({
+      title: '----',
+      artworkUrl100: '',
+      artist: '----',
+      imageLinks: [],
+      albumName: '',
+      trackTimeMillis: 1000,
+      writer: '----',
+      singer: '----',
+      itunesUrl: '',
+      time: 1620900279274,
+      wordCounts: {},
+      icy: '',
+      composer: '',
+      wordCountsAna: [{ name: '----', count: 0, label: '----' }],
+    })
+}
+
+main()
