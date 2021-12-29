4d22fb39ae3e17e8a4db672e8ae0e432d41d038c
vipzero <hiroflighter@gmail.com>
2021-12-29 12:24:19 +0900

    change threadUrls style

--- scripts/threadUrls.ts
+++ scripts/threadUrls.ts
@@ -1,16 +1,9 @@
-export const loadingUrls = `
-`
-  .trim()
+import { readFileSync } from 'fs'
+
+export const loadingUrls = readFileSync(
+  __dirname + '/threadUrls.lines.txt',
+  'utf8'
+)
   .split('\n')
   .filter(Boolean)
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628262507
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628333320
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628347791
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628413179
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628379119
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628427158
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628250138
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628287442
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628440944
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628519885
-// https://hebi.5ch.net/test/read.cgi/news4vip/1628519885
+  .filter((line) => !line.startsWith('//'))
