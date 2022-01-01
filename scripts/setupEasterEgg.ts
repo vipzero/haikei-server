e76d87670a2e02138ea6a2c569e8d709516c1848
vipzero <hiroflighter@gmail.com>
2022-01-01 18:30:52 +0900

    add cvote saekano monogatari

--- scripts/setupEasterEgg.ts
+++ scripts/setupEasterEgg.ts
@@ -4,27 +4,22 @@ const eventId = process.env.EVENT_ID
 async function main() {
   if (!eventId) return
   const votes = {
-    gotoyome: {
-      '1': 0,
-      '2': 0,
-      '3': 0,
-      '4': 0,
-      '5': 0,
-    },
-    oreimo: {
-      ki: 0,
-      ku: 0,
-      ay: 0,
-      ba: 0,
-      ka: 0,
-      ma: 0,
-      se: 0,
-    },
-    oregairu: {
-      yk: 0,
-      yi: 0,
-      ir: 0,
-      ot: 0,
+    gotoyome: { '1': 0, '2': 0, '3': 0, '4': 0, '5': 0 },
+    oreimo: { ki: 0, ku: 0, ay: 0, ba: 0, ka: 0, ma: 0, se: 0 },
+    oregairu: { yk: 0, yi: 0, ir: 0, ot: 0 },
+    saekano: { mg: 0, ut: 0, er: 0, iz: 0, mt: 0 },
+    monogatari: {
+      ht: 0,
+      my: 0,
+      kn: 0,
+      nd: 0,
+      hn: 0,
+      sn: 0,
+      kr: 0,
+      tk: 0,
+      on: 0,
+      ou: 0,
+      oi: 0,
     },
   }
 
