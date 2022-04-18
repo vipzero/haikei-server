c0061c3b0533bce1fa599eabc1c4259ef7a2a2a0
vipzero <hiroflighter@gmail.com>
2022-04-19 00:53:30 +0900

    use swc

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const config = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/*.test.js', '**/*.test.ts', '**/*.test.tsx'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transformIgnorePatterns: ['/node_modules/'],
+
+  transform: {
+    '.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        sourceMaps: true,
+        module: { type: 'commonjs' },
+        jsc: {
+          parser: { syntax: 'typescript', tsx: true },
+          transform: { react: { runtime: 'automatic' } },
+        },
+      },
+    ],
+  },
+}
+
+module.exports = config
