11af8694482685ccfb1f4987b4d2d5b0e769e4f2
vipzero <hiroflighter@gmail.com>
2022-04-29 14:26:20 +0900

    add alogrithm description

--- src/utils/makeSearchWord.ts
+++ src/utils/makeSearchWord.ts
@@ -2,10 +2,16 @@ import { getSyncConf } from './syncConf'
 import { SongSeed } from '../types/index'
 import { pickCharaIcy } from '.'
 
+/* anison DB から曲情報を取れた場合アニメタイトル、カテゴリ(ゲーム,アニメ,SF,映画)などを使用できる */
+/* アニメカテゴリ場合は↓を検索クエリに追加する */
 const animeExt = ['アニメ', 'meme', 'キャプ画', 'キャラ', 'かわいい'].join(
   ' OR '
 )
-const gameExt = ['ゲーム'].join(' OR ') // 実験
+
+/* ゲームカテゴリ場合は↓を検索クエリに追加する */
+const gameExt = ['ゲーム'].join(' OR ')
+
+/* アニメ情報が取れなかった曲は icy に↓を検索クエリに追加する */
 const icyOpt = `(名シーン OR キャラ) (キャプ画像 OR 壁紙) かわいい`
 
 export function makeSearchQuery(song: SongSeed): string {
@@ -21,7 +27,6 @@ export function makeSearchQuery(song: SongSeed): string {
 
   if (!category) return animeTitle
   if (category.includes('アニメ')) {
-    // 実験
     return `${animeTitle} AND (${animeExt})`
   }
   if (category.includes('ゲーム')) {
