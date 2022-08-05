ba5c10da3253771dbbcef668866300aedc3adc54
vipzero <hiroflighter@gmail.com>
2022-08-05 22:04:20 +0900

    update ee

--- scripts/setupEasterEgg.ts
+++ scripts/setupEasterEgg.ts
@@ -47,13 +47,11 @@ async function main() {
     yamajo: { ur: 0, nn: 0, mi: 0, mr: 0, na: 0, as: 0, si: 0 },
     bryunhild: { nk: 0, kn: 0, kz: 0, kt: 0, nn: 0, ht: 0, mt: 0 },
     // prettier-ignore
-    imasmlpr: { mr: 0, sy: 0, kt: 0, ar: 0, kn: 0, em: 0, um: 0, mn: 0, nr: 0, no: 0, mt: 0, ik: 0, yr: 0, },
-    // prettier-ignore
-    imasmlfa: { jr: 0, tm: 0, sz: 0, sb: 0, ay: 0, mz: 0, ro: 0, sh: 0, mm: 0, em: 0, cz: 0, rk: 0, tg: 0, },
-    // prettier-ignore
-    imasmlan: { hn: 0, sr: 0, an: 0, tm: 0, ri: 0, el: 0, ak: 0, kn: 0, tb: 0, kl: 0, hk: 0, my: 0, ko: 0, },
-    // prettier-ignore
-    imasmlau: { ah: 0, ch: 0, yh: 0, yy: 0, rt: 0, az: 0, io: 0, ma: 0, am: 0, mm: 0, mi: 0, hb: 0, tn: 0, },
+    imasml: {
+      mr: 0, sy: 0, kt: 0, ar: 0, km: 0, er: 0, um: 0, mn: 0, nr: 0, no: 0, mt: 0, ik: 0, yr: 0,
+      jr: 0, tk: 0, sz: 0, sb: 0, ay: 0, mz: 0, ro: 0, sh: 0, mk: 0, em: 0, cz: 0, rk: 0, tg: 0,
+      hn: 0, sr: 0, an: 0, tm: 0, ri: 0, el: 0, ak: 0, kn: 0, tb: 0, kl: 0, hk: 0, my: 0, ko: 0,
+      ah: 0, ch: 0, yh: 0, yy: 0, rt: 0, az: 0, io: 0, ma: 0, am: 0, mm: 0, mi: 0, hb: 0, tn: 0, },
     // prettier-ignore
     rakupro: { mm: 0, si: 0, kt: 0, ym: 0, mk: 0, mo: 0, nn: 0, rr: 0, rt: 0, ma: 0, rn: 0, sz: 0, },
     // prettier-ignore
