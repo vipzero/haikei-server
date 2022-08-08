a1e322c74c8fd537787a2ff6941a1f063f32ae86
vipzero <hiroflighter@gmail.com>
2022-08-08 17:00:49 +0900

    fix emol text

--- src/service/emol.ts
+++ src/service/emol.ts
@@ -4,8 +4,12 @@ export const makeEmol = async (lyric: string | null) => {
   if (lyric === null) return { text: '' }
   const text = (
     await Promise.all(
-      lyric.split('\n').map((line) => (line ? emojify(line) : ''))
+      lyric
+        .split('\n')
+        .map((line) => (line ? emojify(line, { onlyEmoji: true }) : ''))
     )
-  ).join('\n')
+  )
+    .filter(Boolean)
+    .join('\n')
   return { text }
 }
