9a13eda4b89915d1b97139cb1de4307743824759
vipzero <hiroflighter@gmail.com>
2023-08-07 10:57:47 +0900

    add status load

--- src/tests/addEe.test.ts
+++ src/tests/addEe.test.ts
@@ -2,12 +2,12 @@ import { Song } from '../types'
 import { addEe } from '../utils/addEe'
 
 test('convertTimeTags', () => {
-  expect(addEe(({ icy: 'a - b' } as Song))).toMatchInlineSnapshot(`
+  expect(addEe({ icy: 'a - b' } as Song)).toMatchInlineSnapshot(`
 {
   "icy": "a - b",
 }
 `)
-  expect(addEe(({ icy: 'KING of SPADE - abc' } as Song))).toMatchInlineSnapshot(`
+  expect(addEe({ icy: 'KING of SPADE - abc' } as Song)).toMatchInlineSnapshot(`
 {
   "hedwig": "mts10:00010",
   "icy": "KING of SPADE - abc",
--- src/utils/addEe.ts
+++ src/utils/addEe.ts
@@ -1,5 +1,24 @@
+import { existsSync, readFileSync, writeFileSync } from 'fs'
 import { titleKeyNormalize } from '../anisonDb/anisonDb'
 import { Song } from '../types'
+const eventId = process.env.EVENT_ID
+
+const statusFile = `data/status.${eventId}.json`
+type Status = {
+  mts: string
+}
+const initialStatus: Status = {
+  mts: '00000',
+}
+const saveStatus = (status: Status) => {
+  writeFileSync(statusFile, JSON.stringify(status))
+}
+
+const loadStatus = (): Status => {
+  if (!existsSync(statusFile)) saveStatus(initialStatus)
+
+  return JSON.parse(readFileSync(statusFile, 'utf-8')) as Status
+}
 
 export const mtsTitles = [
   'Crossing!',
@@ -8,23 +27,22 @@ export const mtsTitles = [
   'KING of SPADE',
   'ESPADA',
 ].map(titleKeyNormalize)
+const charPut = (s: string, i: number, c: string) =>
+  s.substring(0, i) + c + s.substring(i + 1)
 
 export function addEe(song: Song): Song {
-  let hit = false
-  let k = '00000'
-
   song.icy.split(' - ').forEach((title) => {
     const titleNorm = titleKeyNormalize(title)
 
     const t = mtsTitles.findIndex((v) => v === titleNorm)
-    if (t !== -1) {
-      hit = true
-      k = k.substring(0, t) + '1' + k.substring(t + 1)
-    }
+    if (t === -1) return
+
+    const status = loadStatus()
+    status.mts = charPut(status.mts, t, '1')
+    saveStatus(status)
+
+    song.hedwig = `mts10:${charPut(status.mts, t, '1')}`
   })
-  if (hit) {
-    song.hedwig = `mts10:${k}`
-  }
 
   return song
 }
