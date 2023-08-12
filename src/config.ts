4583e7c2d30cec08cb904fd49b54318c6093faf0
vipzero <hiroflighter@gmail.com>
2023-08-12 17:09:41 +0900

    update test

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,8 @@ if (!SERVICE_ACCOUNT_FILE_PATH || !EVENT_ID) {
   process.exit(1)
 }
 
+const isTest = process.env.NODE_ENV === 'test'
 export const serviceAccountPath = SERVICE_ACCOUNT_FILE_PATH
-export const eventId = EVENT_ID
+export const eventId = isTest ? '9999test' : EVENT_ID
 export const enableMobileImg =
   process.env.ENABLE_MOBILE_IMG === '1' && process.env.DIRECT_MODE !== '1'
--- src/tests/addEe.test.ts
+++ src/tests/addEe.test.ts
@@ -8,29 +8,29 @@ test('convertTimeTags', () => {
 }
 `)
 
-  expect(addEe({ icy: 'Crossing! - abc' } as Song)).toMatchInlineSnapshot(`
+  expect(addEe(({ icy: 'Crossing! - abc' } as Song))).toMatchInlineSnapshot(`
 {
-  "hedwig": "mts10:10011",
+  "hedwig": "mts10:10011::AAAA==,AAAAA=,AAAAAAAAAA=",
   "icy": "Crossing! - abc",
 }
 `)
-  expect(addEe({ icy: 'ESPADA - abc' } as Song)).toMatchInlineSnapshot(`
+  expect(addEe(({ icy: 'ESPADA - abc' } as Song))).toMatchInlineSnapshot(`
 {
-  "hedwig": "mts10:10011:MTS",
+  "hedwig": "mts10:10011:MTS:AAAA==,AAAAA=,AAAAAAAAAA=",
   "icy": "ESPADA - abc",
 }
 `)
-  expect(addEe({ icy: '絵本 - hit' } as Song)).toMatchInlineSnapshot(`
+  expect(addEe(({ icy: '絵本 - hit' } as Song))).toMatchInlineSnapshot(`
 {
-  "hedwig": "mts10:10011:LTH",
+  "hedwig": "mts10:10011:LTH:AAAA==,AAAAA=,AAAAAAAAAA=",
   "icy": "絵本 - hit",
 }
 `)
   const icy =
     '咲くは浮世の君花火 - 閃光☆HANABI団 [高山紗代子 (CV.駒形友梨)、高坂海美 (CV.上田麗奈)、福田のり子 (CV.浜崎奈々)、横山奈緒 (CV.渡部優衣)、佐竹美奈子 (CV.大関英里)]'
-  expect(addEe({ icy } as Song)).toMatchInlineSnapshot(`
+  expect(addEe(({ icy } as Song))).toMatchInlineSnapshot(`
 {
-  "hedwig": "mts10:10011:MTG",
+  "hedwig": "mts10:10011:MTG:AAAA==,AAAAA=,AAAAAAAAAA=",
   "icy": "咲くは浮世の君花火 - 閃光☆HANABI団 [高山紗代子 (CV.駒形友梨)、高坂海美 (CV.上田麗奈)、福田のり子 (CV.浜崎奈々)、横山奈緒 (CV.渡部優衣)、佐竹美奈子 (CV.大関英里)]",
 }
 `)
