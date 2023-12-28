8dc1a47993fe76594e4606dee40770a7523c078a
vipzero <hiroflighter@gmail.com>
2023-12-29 00:45:32 +0900

    add patch

--- scripts/check-names.ts
+++ scripts/check-names.ts
@@ -1,4 +1,5 @@
 import { imasIdols } from '../src/utils/addEe'
+import { base64toBools, boolsToBase64 } from '../src/utils/iamsEeNames'
 // import { membersByUnitNameLib } from '../src/utils/iamsEeNames'
 import { log } from '../src/utils/logger'
 import { loadStatus, saveStatus } from '../src/utils/status'
@@ -8,6 +9,32 @@ function main() {
   log(imasIdols(['天海 春香'], 'Jupiter'))
 
   const status = loadStatus()
+  const patchUnits = `
+イルミネーションスターズ
+コメティック
+シーズ
+ノクチル
+放課後クライマックスガールズ
+S.E.M
+Beit
+Jupiter
+`
+    .trim()
+    .split('\n')
+
+  const idols = patchUnits
+    .map((u) => imasIdols([], u))
+    .flat()
+    .filter((v): v is [number, number] => !!v)
+  log(idols)
+  const bins = status.idols.map(base64toBools)
+  log(status.idols)
+
+  idols.forEach(([i, j]) => {
+    bins[i]![j] = true
+  })
+  status.idols = bins.map(boolsToBase64)
+  log(status.idols)
 
   saveStatus(status)
 }
