5de638fb92cc21426a1edad73a5fcd3da41c991d
vipzero <hiroflighter@gmail.com>
2024-01-05 22:14:45 +0900

    update parseIcy

--- src/tests/parseIcy.test.ts
+++ src/tests/parseIcy.test.ts
@@ -1,22 +1,31 @@
 import { icyCaptureAdditionals } from '../utils/icyParse'
 
 test('icyCaptureAdditionals', () => {
-  expect(icyCaptureAdditionals('a - b')).toMatchInlineSnapshot(`null`)
+  expect(icyCaptureAdditionals('a - b')).toMatchInlineSnapshot(`
+{
+  "add": null,
+  "icy": "a - b",
+  "rep": null,
+}
+`)
   expect(icyCaptureAdditionals('a - b{a}')).toMatchInlineSnapshot(`
 {
   "add": "a",
-  "rep": undefined,
+  "icy": "a - b",
+  "rep": null,
 }
 `)
   expect(icyCaptureAdditionals('a - b【k】')).toMatchInlineSnapshot(`
 {
   "add": "k",
-  "rep": undefined,
+  "icy": "a - b",
+  "rep": null,
 }
 `)
   expect(icyCaptureAdditionals('a - b{{k}}')).toMatchInlineSnapshot(`
 {
-  "add": undefined,
+  "add": null,
+  "icy": "a - b",
   "rep": "k",
 }
 `)
--- src/utils/icyParse.ts
+++ src/utils/icyParse.ts
@@ -1,8 +1,14 @@
+import { warn } from './logger'
+
 export const icyCaptureAdditionals = (icy: string) => {
   const m = icy.match(
-    /(?<p1>.*) - (?<p2>.*?)(?:{{(?<rep>.*)}}|(?:\{(?<add>.*)\}|【(?<add2>.*)】))/
+    /(?<icy>(?<p1>.*) - (?<p2>.*?))(?:{{(?<rep>.*)}}|(?:\{(?<add>.*)\}|【(?<add2>.*)】))/
   )
-  if (!m || !m.groups) return null
-  const { rep, add, add2 } = m.groups
-  return { rep, add: add || add2 }
+  if (!m || !m.groups) return { icy, rep: null, add: null }
+  const { rep, add, add2, icy: icyPart } = m.groups
+  if (!icyPart) {
+    warn(`icyParse: icyPart is null ${icyPart}`)
+    return { icy, rep: null, add: null }
+  }
+  return { icy: icyPart, rep: rep || null, add: add || add2 || null }
 }
