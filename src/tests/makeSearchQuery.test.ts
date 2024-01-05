21b517d36167341378d57520353b404db3ed3f15
vipzero <hiroflighter@gmail.com>
2024-01-05 21:55:46 +0900

    format

--- src/tests/makeSearchQuery.test.ts
+++ src/tests/makeSearchQuery.test.ts
@@ -46,18 +46,20 @@ describe('makeSearchQuery', () => {
   })
   test('has animeTitle and category anime', () => {
     expect(
-makeSearchQuery(
-{
-  artist: 'artist',
-  title: 'title',
-  animeTitle: 'アニメタイトルあり',
-  category: 'ほにゃららアニメ',
-  icy: 'artist - title' },
+      makeSearchQuery(
+        {
+          artist: 'artist',
+          title: 'title',
+          animeTitle: 'アニメタイトルあり',
+          category: 'ほにゃららアニメ',
+          icy: 'artist - title',
+        },
 
-
-seed)).
-
-toMatchInlineSnapshot(`"アニメタイトルあり アニメ -公式 AND (ネタ画像 OR 壁紙 OR 作画 OR 5話)"`)
+        seed
+      )
+    ).toMatchInlineSnapshot(
+      `"アニメタイトルあり アニメ -公式 AND (ネタ画像 OR 壁紙 OR 作画 OR 5話)"`
+    )
   })
   test('has animeTitle and category game', () => {
     expect(
@@ -80,10 +82,10 @@ toMatchInlineSnapshot(`"アニメタイトルあり アニメ -公式 AND (ネ�
 
 test('chara pick', () => {
   expect(
-pickCharaIcy(
-'大槻唯 (CV: 山下七海), 緒方智絵里 (CV: 大空直美) & 新田美波 (CV: 洲崎 綾) - 銀のイルカと熱い風')).
-
-toMatchInlineSnapshot(`
+    pickCharaIcy(
+      '大槻唯 (CV: 山下七海), 緒方智絵里 (CV: 大空直美) & 新田美波 (CV: 洲崎 綾) - 銀のイルカと熱い風'
+    )
+  ).toMatchInlineSnapshot(`
 [
   "大槻唯",
   "緒方智絵里",
